1f2d04600a76fdb6284bae187445241a7310a72e
royagustafson <4mXDIFNM1Kz8>
2018-02-09 20:06:31 -0500

    Initialized project structure, maven 'pom.xml'

---
+++ GroutFit/src/test/html/IndexTest.java
@@ -0,0 +1,6 @@
+package html;
+
+
+
+public class IndexTest {
+}
---
+++ GroutFit/src/test/java/MainTest.java
@@ -0,0 +1,25 @@
+package java;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Before
+    public void setUp() throws Exception {
+        // example
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        // example
+    }
+
+    @Test
+    public void main() {
+        // example
+    }
+}
