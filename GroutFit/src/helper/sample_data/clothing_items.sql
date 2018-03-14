7f9fffb51174e308d1072efa87925a8711a71b5b
royagustafson <4mXDIFNM1Kz8>
2018-03-14 17:57:13 -0400

    Incorrectly formatted data, starting point

---
+++ GroutFit/src/helper/sample_data/clothing_items.sql
@@ -0,0 +1,2 @@
+INSERT INTO clothing_item (item_id, type_id, color, size, quantity, gender) VALUES
+																																																																																																																																																																																																																						;
