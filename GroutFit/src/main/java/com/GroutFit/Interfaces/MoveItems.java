ad0e5042702800887e6baae31716367c0d156839
Matthew Ciraula <mac365@pitt.edu>
2018-03-15 15:23:46 -0400

    Please tell me this moves the files

---
+++ GroutFit/src/main/java/com/GroutFit/Interfaces/MoveItems.java
@@ -0,0 +1,26 @@
+package com.GroutFit.Interfaces;
+
+import java.util.ArrayList;
+
+public interface MoveItems {
+	// saves outfit with provided items (may use null if item is not included)
+	void createOutfit(ClothingItem shirt, ClothingItem pants, ClothingItem jacket);
+
+	// posts outfit to feed
+	void postOutfit(Outfit outfit);
+
+	// moves items from cart to wishlist
+	void moveFromCartToWishlist(ArrayList<ClothingItem> items);
+
+	// moves items from wishlist to cart
+	void moveFromWishlistToCart(ArrayList<ClothingItem> items);
+
+	// adds outfit to user's wishlist
+	void addOutfitToWishlist(Outfit outfit);
+
+	// adds outfit to cart
+	void addOutfitToCart(Outfit outfit);
+
+	// begin checkout with items in cart
+	void proceedToCheckout(ArrayList<ClothingItem> cart);
+}
---
+++ GroutFit/src/main/java/com/GroutFit/Interfaces/WishlistInterface.java
@@ -0,0 +1,9 @@
+package com.GroutFit.Interfaces;
+
+public interface WishlistInterface {
+    // add item to wishlist
+    void add(ClothingItem item);
+
+    // remove item from wishlist
+    void remove(ClothingItem item);
+}
