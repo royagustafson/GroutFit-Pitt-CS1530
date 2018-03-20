ade2ec6a360fba24106e46ed4e0ae5e471fda6b2
royagustafson <4mXDIFNM1Kz8>
2018-03-20 15:49:04 -0400

    Refactored according to spark requirements

--- GroutFit/src/main/java/com/GroutFit/Main.java
+++ GroutFit/src/main/java/com/GroutFit/Main.java
@@ -2,8 +2,54 @@ package com.GroutFit;
 
 import static spark.Spark.*;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
+
+        // Perform setup, load public files
+        SessionFactory sf = new Configuration()
+                .configure()
+                .buildSessionFactory();
+        staticFiles.location("/public");
+
+        // Example routes
+
         get("/hello", (req, res) -> "Hello World");
+
+        // Api calls
+
+        path("/api", () -> {
+            path("/cart", () -> {
+
+            });
+            path("/wishlist", () -> {
+
+            });
+        });
+
+        /*
+        get("/", (request, response) -> {
+            // Show something
+        });
+
+        post("/", (request, response) -> {
+            // Create something
+        });
+
+        put("/", (request, response) -> {
+            // Update something
+        });
+
+        delete("/", (request, response) -> {
+            // Annihilate something
+        });
+
+        options("/", (request, response) -> {
+            // Appease something
+        });
+
+        */
     }
 }
