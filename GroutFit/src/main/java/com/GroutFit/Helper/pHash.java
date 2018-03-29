3074d16c28684757f96f1006d95389de8bc05611
royagustafson <4mXDIFNM1Kz8>
2018-03-29 11:35:26 -0400

    Added hash/verify methods

--- GroutFit/src/main/java/com/GroutFit/Helper/pHash.java
+++ GroutFit/src/main/java/com/GroutFit/Helper/pHash.java
@@ -1,60 +1,58 @@
+package com.GroutFit.Helper;
+
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
 public class pHash {
-	public static void main(String[] args) {
-		try {
-			byte[] salt = getSalt();
-			String pw = getPass("password", salt);
-			System.out.println(checkPass("password", pw, salt));
-		} catch(NoSuchAlgorithmException e) {
-			System.out.println("ERROR main");
-		}
-	}
 
-	public static String getPass(String ogPassword, byte[] salt) {
-		String securePass = null;
-		try {
-			MessageDigest md = MessageDigest.getInstance("SHA-256");
-			md.update(salt);
-			byte[] pwByte = md.digest(ogPassword.getBytes());
-			// convert hash to hex
-			StringBuilder sb = new StringBuilder();
-			for(int i=0; i < pwByte.length; i++)
-			    sb.append(Integer.toString((pwByte[i] & 0xff) + 0x100, 16).substring(1));
-			securePass = sb.toString();
-		} catch(NoSuchAlgorithmException e) {
-			System.out.println("ERROR getpass");
-		}
-		return securePass;
-	}
+    public static String hash(String password) {
+        try {
+            return getPass(password, getSalt());
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("SHA-256 not found for hashing");
+            return null;
+        }
+    }
 
-  public static byte[] getSalt() throws NoSuchAlgorithmException {
-      SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
-      sr.setSeed(System.nanoTime());
-      byte[] salt = new byte[16];
-      sr.nextBytes(salt);
-      return salt;
-  }
+    public static Boolean verify(String password, String hashed) {
+        String[] parts = hashed.split("...");
+        try {
+            return checkPass(password, parts[1], parts[0].getBytes());
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("SHA-256 not found for verification");
+            return null;
+        }
+    }
 
-  public static boolean checkPass(String pw, String hashedPW, byte[] salt) {
-  	try {
-  		MessageDigest md = MessageDigest.getInstance("SHA-256");
-  		md.update(salt);
-  		byte[] pwByte = md.digest(pw.getBytes());
-  		// convert hash to hex
-  		StringBuilder sb = new StringBuilder();
-  		for(int i=0; i < pwByte.length; i++)
-  		    sb.append(Integer.toString((pwByte[i] & 0xff) + 0x100, 16).substring(1));
-  		String h = sb.toString();
-  		if(sb.toString().equals(hashedPW))
-  			return true;
-  		else
-  			return false;
-  	} catch(NoSuchAlgorithmException e) {
-			System.out.println("ERROR checkpass");
-			return false;
-		}
-  }
+    private static String getPass(String ogPassword, byte[] salt) throws NoSuchAlgorithmException{
+        String securePass = null;
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update(salt);
+        byte[] pwByte = md.digest(ogPassword.getBytes());
+        // convert hash to hex
+        StringBuilder sb = new StringBuilder();
+        for (byte aPwByte : pwByte) sb.append(Integer.toString((aPwByte & 0xff) + 0x100, 16).substring(1));
+        securePass = new String(salt) + "..." + sb.toString();
+        return securePass;
+    }
+
+    private static byte[] getSalt() {
+        SecureRandom sr = new SecureRandom();
+        sr.setSeed(System.nanoTime());
+        byte[] salt = new byte[sr.nextInt(10) + 10];
+        sr.nextBytes(salt);
+        return salt;
+    }
+
+    private static Boolean checkPass(String pw, String hashedPW, byte[] salt) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update(salt);
+        byte[] pwByte = md.digest(pw.getBytes());
+        // convert hash to hex
+        StringBuilder sb = new StringBuilder();
+        for (byte aPwByte : pwByte) sb.append(Integer.toString((aPwByte & 0xff) + 0x100, 16).substring(1));
+        String h = sb.toString();
+        return sb.toString().equals(hashedPW);
+    }
 }
