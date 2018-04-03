4c46b1246e399db7b25a2b2324d20a47e975f9b0
Spencer Rakoczy <sjr62@pitt.edu>
2018-04-02 23:04:31 -0400

    Added some more sugar to the login pages

--- GroutFit/src/main/resources/public/js/clientControl.js
+++ GroutFit/src/main/resources/public/js/clientControl.js
@@ -1,5 +1,14 @@
-function submitform() {
-    alert("Sending Json");
+function submitLogin() {
+    /*alert("Sending Json");*/
+    var xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action, true);
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    var formData = JSON.stringify($("#myForm").serializeArray());
+    xhr.send(formData);
+}
+
+function submitRegister() {
+    /*alert("Sending Json");*/
     var xhr = new XMLHttpRequest();
     xhr.open(form.method, form.action, true);
     xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
