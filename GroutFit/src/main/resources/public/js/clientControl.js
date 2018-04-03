56a1426722f15c8cd5f972c5ea3a7e50d92cb69e
Spencer Rakoczy <sjr62@pitt.edu>
2018-04-02 22:45:01 -0400

    restored UI from backup. Working in intelliJ now

---
+++ GroutFit/src/main/resources/public/js/clientControl.js
@@ -0,0 +1,8 @@
+function submitform() {
+    alert("Sending Json");
+    var xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action, true);
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    var formData = JSON.stringify($("#myForm").serializeArray());
+    xhr.send(formData);
+}
